0dba418d7669c894a5e56db5542454f69cfc3a4b
toshio.nakashima <toshio.nakashima@icloud.com>
2020-09-16 02:15:50 +0900

    Lesson 1 version 1.02

--- django_lesson/settings.py
+++ django_lesson/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'en@8hjofqkvw(!p-4+n%4h^x7h_ux@za$06hipd-(eb7!h2is&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 # ALLOWED_HOSTS = []
 ALLOWED_HOSTS = ('localhost', '127.0.0.1')
